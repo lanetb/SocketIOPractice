61f427d60d10f232c7e025d7d42445bdfd13b178
lanetb <lanetb@dukes.jmu.edu>
2023-02-13 19:13:14 -0500

    added broadcasting functionality

--- index.js
+++ index.js
@@ -17,11 +17,17 @@ io.on('connection', (socket) => {
 });
 
 io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-  });
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+    });
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
